3b5c022581c26c404f341235a31f78e15e895fcf
Annie Li <annielawang@gmail.com>
2022-11-27 13:00:04 -0700

    added server.js code

---
+++ Develop/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const api = require("./src/index");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// Fallback route for when a user attempts to visit routes that don't exist
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`Listening for requests on port http://localhost:${PORT} 🏎️`)
+);
